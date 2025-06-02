f39b16766f72f2705543037169536cce7b60a345
Cong <you@example.com>
2025-06-02 11:54:06 +0700

    song service

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import doetnv from "dotenv";
+import songRoutes from "./route.js";
+import redis from "redis";
+import cors from "cors";
+
+doetnv.config();
+
+export const redisClient = redis.createClient({
+  password: process.env.Redis_Password,
+  socket: {
+    host: "redis-10845.c1.ap-southeast-1-1.ec2.redns.redis-cloud.com",
+    port: 10845,
+  },
+});
+
+redisClient
+  .connect()
+  .then(() => console.log("connected to redis"))
+  .catch(console.error);
+
+const app = express();
+
+app.use(cors());
+
+app.use("/api/v1", songRoutes);
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
